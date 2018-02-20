9c299bbf34360f29749a7b78fb0ac3f3d33748ef
BlueFire9020 <zacha9020@gmail.com>
2018-02-19 22:10:54 -0500

    Updating Hat Creator
    Still unfinished hat creator, updating to show progress.

--- HatStandalone.cs
+++ HatStandalone.cs
@@ -9,14 +9,45 @@ using System.Reflection;
 //using Witty.Joke.Except.ImNotFunny;
 
 /*
- * Hat creator is NOWHERE NEAR DONE, in case you couldn't tell. It's coming along, don't worry :)
  * 
  * Credit to Nelson Sexton for Unturned and the modding community. Awesome game, my friend! 
  * */
+public enum HATMESHTYPE
+{
+	Custom = 0,
+	Beret = 1,
+	Bowler = 2,
+	Coalition = 3,
+	Engineer = 4,
+	Farmer = 5,
+	Fedora = 6,
+	Fez = 7,
+	Fireman = 8,
+	Fishing = 9,
+	Gladiator = 10,
+	Headphones = 11,
+	Jester = 12,
+	Magician = 13,
+	Military = 14,
+	News = 15,
+	Pilot = 16,
+	Police = 17,
+	RCMP = 18,
+	Togue = 19,
+	Ushanka = 20,
+	Ushanka_Ruski = 21,
+	Viking = 22,
+	Witch = 23,
+
+
+}
 public class HatStandalone : EditorWindow
 {
+	public HATMESHTYPE hmt;
 	bool importdialog = false;
-
+	Texture2D meshTex;
+	public Mesh hatMesh;
+	GameObject hat, item;
 	[MenuItem("Window/Hat Creator")]
 	static void Init()
 	{
@@ -27,12 +58,49 @@ public class HatStandalone : EditorWindow
 	{
 		if (AssetDatabase.IsValidFolder ("Assets/Hat_Assets")) {
 		} else {
-			//AssetDatabase.ImportPackage ("Assets/ClothingCreator/PKGs/Hat.unitypackage", importdialog);
+			AssetDatabase.ImportPackage ("Assets/ClothingCreator/PKGs/Hat.unitypackage", importdialog);
+		}
+		if (GameObject.Find ("Hat") == null) {
+			UnityEngine.Object prefab = AssetDatabase.LoadAssetAtPath ("Assets/Hat_Assets/Resources/Bundles/Items/Hats/Beret_Forest/Hat.prefab", typeof(GameObject));
+				hat = Instantiate (prefab, Vector3.zero, Quaternion.identity) as GameObject;
+			hat.name = "Hat";
+		}
+		if (GameObject.Find ("Item") == null) {
+			UnityEngine.Object prefab = AssetDatabase.LoadAssetAtPath ("Assets/Hat_Assets/Resources/Bundles/Items/Hats/Beret_Forest/Item.prefab", typeof(GameObject));
+			item = Instantiate (prefab, Vector3.zero, Quaternion.identity) as GameObject;
+			item.name = "Item";
 		}
 	}
 	void OnGUI()
 	{
 		GUILayout.Label ("Currently in Development", EditorStyles.boldLabel);
+		EditorGUILayout.HelpBox ("Select the model you would like to use for your hat below. Choose 'Custom' if you would like to input your own model.", MessageType.Info);
+		hmt = (HATMESHTYPE)EditorGUILayout.EnumPopup ("Hat Type:", hmt);
+		if (hmt == HATMESHTYPE.Custom) {
+			EditorGUILayout.BeginHorizontal ();
+			EditorGUILayout.PrefixLabel ("Custom Mesh");
+			if (GUILayout.Button ("Select", GUILayout.Width (200))) {
+				string path = EditorUtility.OpenFilePanel ("Select Mesh", "", "obj,fbx");
+				if (path.Length != 0) 
+				{
+					if (path.Contains (".fbx")) {
+						File.Copy (path, Application.dataPath + "/Hat_Assets/Game/Sources/Models/Items/Hats/Beret_Forest/Model_0.fbx");
+						hatMesh = (Mesh)AssetDatabase.LoadAssetAtPath ("Assets/Hat_Assets/Game/Sources/Models/Items/Hats/Beret_Forest/Model_0.fbx", typeof(Mesh));
+					}
+					else if (path.Contains (".obj")) {
+						File.Copy (path, Application.dataPath + "/Hat_Assets/Game/Sources/Models/Items/Hats/Beret_Forest/Model_0.obj");
+						hatMesh = (Mesh)AssetDatabase.LoadAssetAtPath ("Assets/Hat_Assets/Game/Sources/Models/Items/Hats/Beret_Forest/Model_0.obj", typeof(Mesh));
+					}
+				}
+				//MeshFilter itemMeshFilter = item.GetComponent<MeshFilter> ();
+				item.GetComponent<MeshFilter>().mesh = hatMesh;
+				hat.GetComponentInChildren<MeshFilter> ().mesh = hatMesh;
+				UnityEditor.AssetDatabase.Refresh ();
+				
+			}
+			EditorGUILayout.EndHorizontal ();
+			EditorGUILayout.HelpBox (".blend files are currently unsupported. Please export them into FBX.", MessageType.Info);
+		}
 	}
 	void OnDestroy()
 	{
